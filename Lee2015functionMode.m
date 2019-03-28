68877da3a9522042f5d3141552eed0167e4117c1
benpolletta <benpolletta@gmail.com>
2019-03-28 15:51:52 -0400

    First commit.

---
+++ Lee2015functionMode.m
@@ -0,0 +1,70 @@
+function [data, name, sim_spec] = Lee2015functionMode(sim_struct) % , simulation) %, varargin)
+
+if nargin < 1; sim_struct = []; end
+if isempty(sim_struct); sim_struct = Lee2015initSimStruct; end
+
+Today = datestr(datenum(date),'yy-mm-dd');
+
+start_dir = pwd;
+cd /projectnb/crc-nak/brpp/Lee_2015
+
+savepath = fullfile(pwd, 'Sims', Today);
+mkdir(savepath);
+
+Now = clock;
+name = sprintf('Lee2015_%g_%g_%.4g', Now(4), Now(5), Now(6));
+
+unpack_sim_struct
+
+sim_spec = Lee2015simSpec(column, ach_flag);
+
+save(fullfile(savepath, [name, '_sim_spec.mat']), 'sim_spec', 'sim_struct', 'vary', 'name');
+
+solver
+
+if cluster_flag
+    
+    data = dsSimulate(sim_spec,'tspan',tspan,'downsample_factor',dsfact,'solver',solver,'coder',0,...
+        'vary',vary,'verbose_flag',1,'cluster_flag',cluster_flag,...
+        'debug_flag',debug_flag,'compile_flag',compile_flag,...
+        'analysis_functions',analysis_functions,'analysis_options',analysis_options,...
+        'overwrite_flag',1,'one_solve_file_flag',1,'qsub_mode',qsub_mode,...
+        'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
+    
+    cd (start_dir)
+    
+    return
+
+else
+    
+    tic;
+    
+    data = dsSimulate(sim_spec,'tspan',tspan,'downsample_factor',dsfact,'solver',solver,'coder',0,...
+        'vary',vary,'verbose_flag',1,'parallel_flag',parallel_flag,'num_cores',num_cores,...
+        'debug_flag',debug_flag,'compile_flag',compile_flag,...
+        'analysis_functions',analysis_functions,'analysis_options',analysis_options,...
+        'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
+    
+    toc;
+        
+end
+
+close('all')
+
+dsPlot(data)
+
+dsPlot(data, 'plot_type', 'rastergram')
+
+figHandles = findobj('Type', 'Figure');
+
+for f = 1:length(figHandles)
+    
+    saveas(figHandles(f), fullfile(savepath, [name, '_', num2str(f), '.fig']))
+
+    save_as_pdf(figHandles(f), fullfile(savepath, [name, '_', num2str(f)]))
+    
+end
+
+cd (start_dir)
+
+end
---
+++ Lee2015simSpec.m
@@ -0,0 +1,299 @@
+function sim_spec = Lee2015simSpec(column, ach_flag)
+
+sim_spec = struct();
+
+pop_list = {'supRS', 'supFS', 'supSI',...
+    'L4RS', 'L4FS',...
+    'deepIBdendrite', 'deepIBsoma', 'deepIBaxon',...
+    'deepRSdendrite', 'deepRSsoma', 'deepRSaxon',...
+    'deepFS', 'deepSI'};
+
+no_pops = length(pop_list);
+
+param_list = {'gleak', 'gM', 'gCaH', 'gCaL', 'gAR', 'Iapp', 'gsyn'};
+
+no_params = length(param_list);
+
+conductance = get_conductance(column, ach_flag);
+
+for pop = 1:no_pops
+
+    sim_spec.populations(pop).equations = pop_list{pop};
+    sim_spec.populations(pop).size = 20;
+    
+    parameters = cell(1, 2*no_params);
+    
+    for param = 1:no_params
+        
+        parameters{2*param - 1} = param_list{param};
+        parameters{2*param} = conductance(param, pop);
+        
+    end
+    
+    sim_spec.populations(pop).parameters = parameters;
+
+end
+
+no_pops = length(pop_list);
+
+mechanisms = {'iSYN', 'iNMDA', 'iGAP'};
+
+[fanout, gSYN] = get_connectivity(column, ach_flag);
+
+subcategories = {'FS', 'SI', 'sup', 'deep', 'IBaxon'};
+
+for s = 1:length(subcategories)
+    
+    eval([subcategories{s}, '_index = contains(pop_list, "', subcategories{s}, '");'])
+
+end
+
+E_index = ~(FS_index | SI_index);
+
+deepEtosupSI = double(deep_index & E_index)'*contains(pop_list, 'supSI');
+
+supEtosupI = double(sup_index & E_index)'*double(sup_index & (FS_index | SI_index));
+
+GJ = zeros(no_pops, no_pops);
+
+switch column
+    
+    case 'par'
+        
+        GJ = double(IBaxon_index)'*IBaxon_index;
+        gNMDA = FS_index*.01 + SI_index*.05;
+        
+    case 'a1_2015'
+        
+        gNMDA = (FS_index + SI_index)*.01;
+        
+    case 'a1_2013'
+        
+        gNMDA = FS_index*.04 + SI_index*.03;
+        
+end
+
+no_mechanisms = ones(no_pops, no_pops) + supEtosupI + GJ;
+
+ESYN = (FS_index + SI_index)*(-80);
+
+tauRx = repmat((E_index*.25 + (FS_index + SI_index)*.5), no_pops, 1);
+tauRX(logical(deepEtosupSI)) = 2.5;
+
+tauDx = repmat((E_index + FS_index*8 + SI_index*20), no_pops, 1);
+tauDx(logical(deepEtosupSI)) = 50;
+
+C_index = 0;
+
+for p = 1:no_pops
+    
+    for q = 1:no_pops
+        
+        if gSYN(p, q) > 0
+            
+            C_index = C_index + 1;
+            
+            sim_spec.connections(C_index).direction = [pop_list{p}, '->', pop_list{q}];
+            
+            sim_spec.connections(C_index).mechanism_list = mechanisms(1:no_mechanisms(p,q));
+            
+            sim_spec.connections(C_index).parameters = {'gSYN', gSYN(p, q),...
+                'tauDx', tauDx(p, q), 'tauRx', tauRx(p, q),...
+                'ESYN', ESYN(p), 'fanout', fanout(p, q),...
+                'fanoutNMDA', 10, 'gNMDA', supEtosupI(p,q)*gNMDA(q),...
+                'fanoutGAP', 7, 'gGAP', .04*GJ(p,q)};
+            
+        end
+
+    end
+    
+end
+
+multicomp_pops = {'deepIB', 'deepRS'};
+
+compartments = {'dendrite', 'soma', 'axon'};
+
+for p = 1:length(multicomp_pops)
+    
+    pop_name = multicomp_pops{p};
+    
+    for c = 1:(length(compartments) - 1)
+        
+        C_index = C_index + 1;
+        
+        pre_name = [pop_name, compartments{c}];
+        
+        post_name = [pop_name, compartments{c + 1}];
+        
+        sim_spec.connections(C_index).direction = [pre_name, '->', post_name];
+        
+        sim_spec.connections(C_index).mechanism_list = {'iCOM'};
+        
+    end
+    
+end
+
+end
+
+function conductance = get_conductance(column, ach_flag)
+
+switch column
+    
+    case 'a1_2013'
+        
+        conductance = [0.1*ones(1, 13);... % g_L
+            0.5, 0, 8, 0.3, 0, 2, 0, 4, 2, 0, 4, 0, 4;... % g_M
+            zeros(1, 7), 4, 0, 0, 1.6, 0, 0;... % g_CaH
+            zeros(1, 13);... % g_CaL
+            zeros(1, 13);... % g_h
+            0, 0, -1, -1, 2, 1, 1, 2, 1, 1, 2, 0, -1;... % Iapp
+            .2, .02, 0, 1, .03, 0, 0, 3, 0, 0, 3, 0, 0]; % g_ext
+        
+    case 'a1_2015'
+        
+        conductance = [0.1*ones(1, 13);... % g_L
+            0.3, 0, 6, 0.9, 0, 0.3, 0, 0.6, 0.3, 0, 0.6, 0, 3;... % g_M
+            zeros(1, 7), 4, 0, 0, 2, 0, 0;... % g_CaH
+            0, 0, 1, zeros(1, 9), 0.3;... % g_CaL
+            zeros(1, 13);... % g_h
+            -2, 0, 0, -10, -5, 0.8, -1.4, -5, 0.8, -1.4, -5, 4, -4;... % Iapp
+            .5, 0, 0, 0.1, .03, 0, 0, 0.1, 0, 0, 0.1, 0, 0]; % g_ext
+        
+        if ach_flag
+            
+            conductance(2, :) = 2.*conductance(2, :);
+            conductance(6, 3) = 6.6;
+            conductance(6, end - 1) = -1.3;
+            conductance(6, end) = 2.6;
+            
+        end
+        
+    case 'par'
+        
+        conductance = [0.1*ones(1, 4), 0.2, 0.1*ones(1, 8);... % g_L
+            0.6, 0, 3, 1, 0, 8, 0, 2, 8, 0, 3, 0, 3;... % g_M
+            zeros(1, 7), 4, 0, 0, 2, 0, 0;... % g_CaH
+            0, 0, 1, zeros(1, 9), 0.5;... % g_CaL
+            zeros(1, 7), 0.1, 0, 0, 0.1, 0, 0;... % g_h
+            -4, 2, 2, 0, -4, -5, -5, -5, -2, -3, -1, 0, -3;... % Iapp
+            0.6, 0, 0, 0.2, 0.1, zeros(1, 8)]; % g_ext
+        
+        if ach_flag
+            
+            conductance(2, 1) = 0.3;
+            conductance(2, 3) = 6;
+            conductance(2, end) = 6;
+            conductance(6, end) = 3;
+            
+        end
+        
+end
+
+end
+
+function [fanout, gSYN] = get_connectivity(column, ach_flag)
+
+switch column
+    
+    case 'a1_2013'
+        
+        fanout = [5, 10, 10, 0, 0, 20, 20, 0, 0;... % from supRS
+            5, 8, 5, 5, 0, 0, 0, 0, 0;... % from supFS
+            5, 5, 0, 0, 0, 0, 0, 0, 0;... % from supSI
+            5, 0, 0, 10, 10, 10, 10, 20, 0;... % from L4RS
+            5, 0, 0, 10, 10, 0, 0, 0, 0;... % from L4FS
+            0, 2, 2, 0, 0, 10, 10, 10, 10;... % from deepIB
+            0, 2, 2, 0, 0, 10, 10, 10, 10;... % from deepRS
+            0, 0, 0, 0, 0, 20, 20, 20, 10;... % from deepFS
+            0, 0, 0, 0, 10, 20, 10, 10, 20]; % from deepSI
+        
+        gSYN = [.22, .3, .03, 0, 0, .212, .212, 0, 0;... % from supRS
+            .4, .6, .1, .4, 0, 0, 0, 0, 0;... % from supFS
+            .1, .2, 0, 0, 0, 0, 0, 0, 0;... % from supSI
+            .2, 0, 0, .4, .2, .212, .212, .3, 0;... % from L4RS
+            .02, 0, 0, 1, .3, 0, 0, 0, 0;... % from L4FS
+            0, .2, .2, 0, 0, .02, .02, .12, .12;... % from deepIB
+            0, .2, .2, 0, 0, .02, .02, .05, .15;... % from deepRS
+            0, 0, 0, 0, 0, .1, .1, .5, .3;... % from deepFS
+            0, 0, 0, 0, .4, .3, .3, .6, .4]; % from deepSI
+        
+    case 'a1_2015'
+        
+        fanout = [10, 20, 10, 0, 0, 20, 20, 20, 0;... % from supRS
+            10, 10, 10, 8, 0, 0, 0, 0, 0;... % from supFS
+            10, 10, 2, 0, 0, 10, 10, 0, 0;... % from supSI
+            10, 0, 0, 10, 20, 10, 10, 0, 0;... % from L4RS
+            10, 0, 0, 20, 20, 0, 0, 0, 0;... % from L4FS
+            0, 2, 2, 0, 0, 5, 5, 10, 10;... % from deepIB
+            0, 2, 2, 0, 0, 5, 5, 10, 10;... % from deepRS
+            0, 0, 0, 0, 0, 20, 20, 20, 10;... % from deepFS
+            0, 10, 0, 0, 10, 20, 20, 10, 20]; % from deepSI
+        
+        gSYN = [.02, .3, .03, 0, 0, .05, .05, .4, 0;... % from supRS
+            1.2, .2, .6, .03, 0, 0, 0, 0, 0;... % from supFS
+            .02, 1, .2, 0, 0, .01, .01, 0, 0;... % from supSI
+            .3, 0, 0, .03, .25, .01, .01, 0, 0;... % from L4RS
+            .1, 0, 0, .3, .3, 0, 0, 0, 0;... % from L4FS
+            0, .01, .25, 0, 0, .02, .01, .3, .2;... % from deepIB
+            0, .01, .25, 0, 0, .01, .01, .3, .2;... % from deepRS
+            0, 0, 0, 0, 0, .4, .4, .8, .8;... % from deepFS
+            0, .02, 0, 0, .6, .5, .5, .03, .4]; % from deepSI
+        
+        if ach_flag
+            
+            gSYN(5, 4) = 2.*gSYN(5, 4);
+            
+        end
+        
+    case 'par'
+        
+        fanout = [10, 20, 10, 0, 0, 20, 20, 20, 0;... % from supRS
+            10, 10, 10, 8, 0, 0, 0, 0, 0;... % from supFS
+            10, 10, 0, 0, 0, 10, 10, 0, 0;... % from supSI
+            10, 0, 0, 10, 20, 10, 10, 20, 0;... % from L4RS
+            10, 0, 0, 20, 20, 0, 0, 0, 0;... % from L4FS
+            0, 2, 2, 0, 0, 5, 5, 10, 10;... % from deepIB
+            0, 2, 2, 0, 0, 0, 5, 10, 10;... % from deepRS
+            0, 0, 0, 0, 0, 20, 20, 20, 10;... % from deepFS
+            0, 10, 10, 0, 10, 0, 20, 10, 20]; % from deepSI
+        
+        gSYN = [.3, .3, .02, 0, 0, .01, .01, .01, 0;... % from supRS
+            1.4, .5, .4, .2, 0, 0, 0, 0, 0;... % from supFS
+            .05, .2, 0, 0, 0, .1, .1, 0, 0;... % from supSI
+            .05, 0, 0, .02, .1, .01, .01, .02, 0;... % from L4RS
+            .05, 0, 0, .5, .2, 0, 0, 0, 0;... % from L4FS
+            0, .01, .1, 0, 0, .25, .02, .2, .2;... % from deepIB
+            0, .01, .1, 0, 0, 0, .02, .2, .2;... % from deepRS
+            0, 0, 0, 0, 0, .01, .1, .6, .05;... % from deepFS
+            0, .5, .1, 0, .34, 0, .4, .4, .6]; % from deepSI
+        
+        if ach_flag
+            
+            gSYN(5, 4) = 2.*gSYN(5, 4);
+            
+        end
+        
+end
+    
+params = {fanout, gSYN};
+
+for param = 1:length(params)
+    
+    param_mat = params{param};
+    
+    zero_pad = zeros(1, size(param_mat, 1));
+    
+    param_mat = [param_mat(1:5, :); zero_pad; zero_pad; param_mat(6, :);...
+        zero_pad; zero_pad; param_mat(7:end, :)];
+    
+    zero_pad = zeros(size(param_mat, 1), 1);
+    
+    param_mat = [param_mat(:, 1:6), zero_pad, zero_pad, param_mat(:, 7), zero_pad, zero_pad, param_mat(:, 8:end)];
+    
+    params{param} = param_mat;
+    
+end
+
+fanout = params{1}; gSYN = params{2};
+
+end
