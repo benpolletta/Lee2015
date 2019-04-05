b8c51048b76ce0e9a7a2d82d36447a34ce1b45bd
benpolletta <benpolletta@gmail.com>
2019-04-05 16:41:33 -0400

    Modifying iSYN to use makeSYNmask.

--- getPoissonExp.m
+++ getPoissonExp.m
@@ -5,7 +5,7 @@ if nargin<6, dt=.01; end
 if nargin<5, T=1000; end
 if nargin<4, N=1; end
 if nargin<3, Pmax=1; end
-if nargin<2, tauD=10; end
+if nargin<2, tauD=1; end
 if nargin<1, lambda=50; end
 
 % file=sprintf('input_poissonexp_N%g_lambda%g_tau%g_%gms_%g.mat',N,lambda,tauD,T,poiss_id);
