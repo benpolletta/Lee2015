b2e571f6b7dbd17057799e15f10acd2b331db8de
David Stanley <davearthur.stanley@gmail.com>
2019-04-06 19:11:45 -0400

    Updated Lee2015functionMode.m to use DynaSim submodule and search in both Ben and Dave's paths

--- Lee2015functionMode.m
+++ Lee2015functionMode.m
@@ -3,11 +3,16 @@ function [data, name, sim_spec] = Lee2015functionMode(sim_struct) % , simulation
 if nargin < 1; sim_struct = []; end
 if isempty(sim_struct); sim_struct = Lee2015initSimStruct; end
 
+% Setup path
+restoredefaultpath
+addpath(genpath('submodules'));
+
 Today = datestr(datenum(date),'yy-mm-dd');
 
 start_dir = pwd;
 
-% CD_ben;
+% Try-catch loops for changing directories
+CD_ben;
 CD_dave;
 
 
@@ -39,7 +44,7 @@ if cluster_flag
         cd (start_dir)
         
         return
-    
+        
     else
         
         data = dsSimulate(sim_spec,'tspan',tspan,'downsample_factor',dsfact,'solver',solver,'coder',0,...
@@ -55,7 +60,7 @@ if cluster_flag
         
         
     end
-
+    
 else
     
     if isempty(vary), parallel_flag = 0; end
@@ -69,21 +74,51 @@ else
         'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
     
     toc;
-        
+    
 end
 
 dsPlot(data)
-    
+
 saveas(gcf, fullfile(savepath, [name, '.fig']))
 
 save_as_pdf(gcf, fullfile(savepath, name))
 
 dsPlot(data, 'plot_type', 'rastergram')
-    
+
 saveas(gcf, fullfile(savepath, [name, '_raster.fig']))
 
 save_as_pdf(gcf, fullfile(savepath, [name, '_raster']))
 
 cd (start_dir)
 
+end
+
+
+function CD_ben
+    try
+        cd /projectnb/crc-nak/brpp/Lee2015/
+    catch err
+        display(err)
+    end
+
+    try
+        cd /Users/benjaminpittman-polletta/Documents/Science/Research_Projects/Lee2015
+    catch err
+        display(err)
+    end
+
+end
+
+function CD_dave
+    try
+        cd ~/src/Lee2015/
+    catch err
+        display(err)
+    end
+
+    % try
+    %     cd /Users/benjaminpittman-polletta/Documents/Science/Research_Projects/Lee2015
+    % catch err
+    %     display(err)
+    % end
 end
