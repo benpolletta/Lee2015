66529b3f2cd82452287c345a0e4fc37da63e92d0
David Stanley <davearthur.stanley@gmail.com>
2019-04-06 18:13:29 -0400

    Added CD scripts for Ben and Dave

---
+++ CD_ben.m
@@ -0,0 +1,11 @@
+try
+    cd /projectnb/crc-nak/brpp/Lee2015/
+catch err
+    display(err)
+end
+
+try
+    cd /Users/benjaminpittman-polletta/Documents/Science/Research_Projects/Lee2015
+catch err
+    display(err)
+end
